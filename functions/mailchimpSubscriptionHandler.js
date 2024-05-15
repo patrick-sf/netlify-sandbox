b39c795ce4ad891e9064e019810ab991630551a6
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-05-15 16:51:47 -0700

    test: troubleshooting

--- functions/mailchimpSubscriptionHandler.js
+++ functions/mailchimpSubscriptionHandler.js
@@ -1,43 +1,18 @@
-const axios = require("axios");
-// var crypto = require("crypto");
-
 const apiRoot = "https://us17.api.mailchimp.com/3.0/lists/e77cefc9e4/members/";
 
-exports.handler = async (event, context) => {
+exports.handler = async () => {
   try {
-    // const email = event.queryStringParameters.email;
-    // if (!email) {
-    //   return {
-    //     statusCode: 500,
-    //     body: "email query paramter required",
-    //   };
-    // }
-
-    // https://gist.github.com/kitek/1579117
-    // let emailhash = crypto.createHash("md5").update(email).digest("hex");
-
-    return axios({
+    const response = await fetch(apiRoot, {
       method: "get",
-      url: apiRoot,
-      // data:{
-      //   email_address:email,
-      //   status:'subscribed'
-      // },
-      auth: {
-        username: "anythingreally",
-        password: process.env.MAILCHIMP_API_KEY,
-      },
-    })
-      .then((res) => {
-        return {
-          statusCode: 200,
-          body: JSON.stringify(res.data),
-        };
-      })
-      .catch((err) => {
-        console.log("returning from here", err.response.data.detail);
-        return { statusCode: 500, body: JSON.stringify(err.response.data) };
-      });
+      headers: { Authorization: process.env.MAILCHIMP_API_KEY },
+    });
+
+    const responseData = await response.json();
+
+    return {
+      statusCode: 200,
+      body: responseData,
+    };
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
   }
