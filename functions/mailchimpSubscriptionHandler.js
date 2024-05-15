00a6015c417ef527a37a701fc4ff3d603dc53715
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-05-15 16:47:22 -0700

    test: troubleshooting

--- functions/mailchimpSubscriptionHandler.js
+++ functions/mailchimpSubscriptionHandler.js
@@ -1,139 +1,44 @@
-const crypto = require("crypto");
-const mailchimp = require("@mailchimp/mailchimp_marketing");
+const axios = require("axios");
+// var crypto = require("crypto");
 
-mailchimp.setConfig({
-  apiKey: process.env.MAILCHIMP_API_KEY,
-  server: "us17",
-});
+const apiRoot = "https://us17.api.mailchimp.com/3.0/lists/e77cefc9e4/members/";
 
-async function callPing() {
-  const response = await mailchimp.ping.get();
-  console.log(response);
-}
+exports.handler = async (event, context) => {
+  try {
+    // const email = event.queryStringParameters.email;
+    // if (!email) {
+    //   return {
+    //     statusCode: 500,
+    //     body: "email query paramter required",
+    //   };
+    // }
 
-const genericHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  "Access-Control-Allow-Methods": "GET, POST, OPTION",
-};
-const genericError = {
-  statusCode: 400,
-  headers: genericHeaders,
-  body: "Oops! Something went wrong. Please try subscribing again.",
-};
+    // https://gist.github.com/kitek/1579117
+    // let emailhash = crypto.createHash("md5").update(email).digest("hex");
 
-exports.handler = async (event) => {
-  // const response = await fetch(process.env.MAILCHIMP_URL, {
-  //   method: "post",
-  //   body: JSON.stringify({
-  //     ...params,
-  //     status: "pending",
-  //   }),
-  //   headers: myHeaders,
-  //   redirect: "follow",
-  // });
-
-  // const data = await response.json();
-  if (event.httpMethod === "OPTIONS") {
-    return {
-      statusCode: 204,
-      headers: genericHeaders,
-      body: "Successful preflight call.",
-    };
+    return axios({
+      method: "get",
+      url: apiRoot,
+      // data:{
+      //   email_address:email,
+      //   status:'subscribed'
+      // },
+      auth: {
+        username: "anythingreally",
+        password: process.env.MAILCHIMP_API_KEY,
+      },
+    })
+      .then((res) => {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(res.data),
+        };
+      })
+      .catch((err) => {
+        console.log("returning from here", err.response.data.detail);
+        return { statusCode: 500, body: JSON.stringify(err.response.data) };
+      });
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
   }
-
-  const ping = await callPing();
-  return {
-    statusCode: 200,
-    headers: genericHeaders,
-    body: `httpMethod: ${event.httpMethod} || 
-    ping: ${ping}`,
-  };
-  // if (event.httpMethod !== "POST") {
-  //   return {
-  //     statusCode: 405,
-  //     headers: genericHeaders,
-  //     body: "Function not found...",
-  //   };
-  // }
-
-  // const params = JSON.parse(event.body);
-
-  // if (!params.email_address) {
-  //   return genericError;
-  // }
-
-  // const isValidEmail = String(params.email_address)
-  //   .toLowerCase()
-  //   .match(
-  //     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  //   );
-
-  // if (!isValidEmail) {
-  //   return {
-  //     statusCode: 403,
-  //     headers: genericHeaders,
-  //     body: "Please enter a valid email address.",
-  //   };
-  // }
-
-  // const hash = crypto
-  //   .createHash("md5")
-  //   .update(params.email_address)
-  //   .digest("hex");
-  // const myHeaders = new Headers();
-
-  // myHeaders.append("Content-Type", "application/json");
-  // myHeaders.append("Authorization", process.env.MAILCHIMP_API_KEY);
-
-  // try {
-  //   const memberDataResponse = await fetch(
-  //     `${process.env.MAILCHIMP_URL}/${hash}`,
-  //     {
-  //       method: "get",
-  //       headers: myHeaders,
-  //     }
-  //   );
-
-  //   const memberData = await memberDataResponse.json();
-
-  //   if (
-  //     (memberData && memberData.status === "pending") ||
-  //     memberData.status === "subscribed"
-  //   ) {
-  //     return memberData.status === "pending"
-  //       ? {
-  //           statusCode: 409,
-  //           headers: genericHeaders,
-  //           body: "Your subscription is almost complete! Please check your email and click the confirmation link.",
-  //         }
-  //       : {
-  //           statusCode: 409,
-  //           headers: genericHeaders,
-  //           body: "You are already subscribed to our newsletter.",
-  //         };
-  //   }
-
-  //   const response = await fetch(process.env.MAILCHIMP_URL, {
-  //     method: "post",
-  //     body: JSON.stringify({
-  //       ...params,
-  //       status: "pending",
-  //     }),
-  //     headers: myHeaders,
-  //     redirect: "follow",
-  //   });
-
-  //   const data = await response.json();
-
-  //   return data.status === "pending"
-  //     ? {
-  //         statusCode: 200,
-  //         headers: genericHeaders,
-  //         body: "Thank you for subscribing to our newsletter. You should receive a confirmation email soon.",
-  //       }
-  //     : genericError;
-  // } catch (err) {
-  //   return genericError;
-  // }
 };
