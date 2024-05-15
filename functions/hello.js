f97a8bd169821dd8d610e3fcc0fbd8dc8f548aa6
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-05-15 10:22:42 -0700

    feat: use netlify env vars

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,25 @@
-// functions/hello.js
-exports.handler = async (event) => {
-  const subject = event.queryStringParameters.name || "World";
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}!`,
-  };
+exports.handler = async () => {
+  try {
+    const config = {
+      method: "get",
+      url: process.env.MAILCHIMP_URL,
+      headers: {
+        Authorization: process.env.MAILCHIMP_API_KEY,
+      },
+    };
+
+    const user = await axios.get(config.url, {
+      headers: config.headers,
+    });
+
+    console.log(JSON.stringify(user));
+
+    return {
+      statusCode: 200,
+      body: `User: ${JSON.stringify(user)}`,
+    };
+  } catch (err) {
+    console.log(JSON.stringify(err));
+    throw new Error(err);
+  }
 };
