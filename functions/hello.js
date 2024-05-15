77e86f31da2ca86c5ec7360c2fb2fbd372eaa573
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-05-15 11:23:37 -0700

    test: mailchimp post with body

--- functions/hello.js
+++ functions/hello.js
@@ -1,42 +1,30 @@
 exports.handler = async (event) => {
-  // if (event.httpMethod !== "POST") {
-  //   return { statusCode: 405, body: "Function not found..." };
-  // }
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Function not found..." };
+  }
 
   const params = JSON.parse(event.body);
 
+  const myHeaders = new Headers();
+
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", process.env.MAILCHIMP_API_KEY);
+
+  const body = JSON.stringify(params);
+
+  const response = await fetch(process.env.MAILCHIMP_URL, {
+    method: "post",
+    body,
+    headers: myHeaders,
+    redirect: "follow",
+  });
+
+  const data = await response.json();
+
   return {
     statusCode: 200,
-    body: `RESPONSE: ${JSON.stringify(params)}`,
+    body: `URL: ${process.env.MAILCHIMP_URL} ||
+    USER: ${process.env.MAILCHIMP_USER} ||
+    POST DATA: ${JSON.stringify(data)}`,
   };
-
-  // const myHeaders = new Headers();
-
-  // myHeaders.append("Content-Type", "application/json");
-  // myHeaders.append("Authorization", process.env.MAILCHIMP_API_KEY);
-
-  // const body = JSON.stringify({
-  //   email_address: process.env.MAILCHIMP_USER,
-  //   status: "pending",
-  //   merge_fields: {
-  //     FNAME: "Patrick",
-  //     LNAME: "Wu",
-  //   },
-  // });
-
-  // const response = await fetch(process.env.MAILCHIMP_URL, {
-  //   method: "post",
-  //   body,
-  //   headers: myHeaders,
-  //   redirect: "follow",
-  // });
-
-  // const data = await response.json();
-
-  // return {
-  //   statusCode: 200,
-  //   body: `URL: ${process.env.MAILCHIMP_URL} ||
-  //   USER: ${process.env.MAILCHIMP_USER} ||
-  //   POST DATA: ${JSON.stringify(data)}`,
-  // };
 };
