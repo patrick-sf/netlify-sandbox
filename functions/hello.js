4db393c64af63e5cf1d2f2bf323c8cda71c5b486
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-05-15 11:09:06 -0700

    test: inspect event

--- functions/hello.js
+++ functions/hello.js
@@ -1,31 +1,32 @@
-exports.handler = async () => {
-  const myHeaders = new Headers();
+exports.handler = async (event) => {
+  return JSON.stringify(event);
+  // const myHeaders = new Headers();
 
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", process.env.MAILCHIMP_API_KEY);
+  // myHeaders.append("Content-Type", "application/json");
+  // myHeaders.append("Authorization", process.env.MAILCHIMP_API_KEY);
 
-  const body = JSON.stringify({
-    email_address: process.env.MAILCHIMP_USER,
-    status: "pending",
-    merge_fields: {
-      FNAME: "Patrick",
-      LNAME: "Wu",
-    },
-  });
+  // const body = JSON.stringify({
+  //   email_address: process.env.MAILCHIMP_USER,
+  //   status: "pending",
+  //   merge_fields: {
+  //     FNAME: "Patrick",
+  //     LNAME: "Wu",
+  //   },
+  // });
 
-  const response = await fetch(process.env.MAILCHIMP_URL, {
-    method: "post",
-    body,
-    headers: myHeaders,
-    redirect: "follow",
-  });
+  // const response = await fetch(process.env.MAILCHIMP_URL, {
+  //   method: "post",
+  //   body,
+  //   headers: myHeaders,
+  //   redirect: "follow",
+  // });
 
-  const data = await response.json();
+  // const data = await response.json();
 
-  return {
-    statusCode: 200,
-    body: `URL: ${process.env.MAILCHIMP_URL} || 
-    USER: ${process.env.MAILCHIMP_USER} || 
-    POST DATA: ${JSON.stringify(data)}`,
-  };
+  // return {
+  //   statusCode: 200,
+  //   body: `URL: ${process.env.MAILCHIMP_URL} ||
+  //   USER: ${process.env.MAILCHIMP_USER} ||
+  //   POST DATA: ${JSON.stringify(data)}`,
+  // };
 };
