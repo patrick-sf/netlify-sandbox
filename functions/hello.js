6318f31bc12bb6de934a525263b5dd9bb70a48cc
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-05-15 10:51:27 -0700

    test: fetch

--- functions/hello.js
+++ functions/hello.js
@@ -1,11 +1,16 @@
 exports.handler = async (event) => {
   console.log(`url: ${process.env.MAILCHIMP_URL || "not found"}`);
 
+  const body = { a: 1 };
+
   const response = await fetch("https://httpbin.org/post", {
-    method: "POST",
-    body: params,
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
   });
   const data = await response.json();
+
+  console.log(data);
 
   console.log(`POST DATA: ${data}`);
 
