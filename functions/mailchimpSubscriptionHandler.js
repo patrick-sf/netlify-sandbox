063eae96b7fa8a4d7c17d7c6644dbc871560be8a
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-05-20 08:49:43 -0700

    test: troubleshooting

--- functions/mailchimpSubscriptionHandler.js
+++ functions/mailchimpSubscriptionHandler.js
@@ -13,8 +13,8 @@ const genericError = {
 
 exports.handler = async (event, context) => {
   console.log(`Received request: ${event.httpMethod}`);
-  console.log(`Client headers: ${Object.keys(clientContext)}`);
-  console.log(`Identity headers: ${Object.keys(identity)}`);
+  console.log(`Client headers: ${Object.keys(context.clientContext)}`);
+  console.log(`Identity headers: ${Object.keys(context.identity)}`);
 
   if (event.httpMethod === "OPTIONS") {
     return {
