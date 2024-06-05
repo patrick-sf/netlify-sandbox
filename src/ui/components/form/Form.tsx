2b929dba8b8067cab2c00d20e3f5788987d4ea5e
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-06-05 09:30:59 -0700

    test: netlify attribute

--- src/ui/components/form/Form.tsx
+++ src/ui/components/form/Form.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export const Form = () => (
   <div>
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" netlify>
       <p>
         <label>Your Name: <input type="text" name="name" /></label>
       </p>
