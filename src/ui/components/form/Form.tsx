7f9ac1016e3ed3ef5d3cca160a36f13f980f3c94
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-06-05 09:39:08 -0700

    test: submit handler

--- src/ui/components/form/Form.tsx
+++ src/ui/components/form/Form.tsx
@@ -1,26 +1,42 @@
 import * as React from 'react';
 
-export const Form = () => (
-  <div>
-    <form name="contact" method="POST" netlify>
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Your Role: <select name="role[]" multiple>
-          <option value="contractor">Contractor</option>
-          <option value="subcontractor">SubContractor</option>
-        </select></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  </div>
-);
+export const Form = () => {
+  const handleSubmit = (event: { preventDefault: () => void; target: any; }) => {
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData as any).toString(),
+    })
+      .then(() => console.log('success'))
+      .catch((error) => alert(error));
+  };
+  return (
+    <div>
+      <form name="contact" method="POST" netlify>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="contractor">Contractor</option>
+            <option value="subcontractor">SubContractor</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit" onClick={handleSubmit}>Send</button>
+        </p>
+      </form>
+    </div>
+  );
+}
