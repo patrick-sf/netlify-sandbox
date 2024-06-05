0a478368c3070b8414401b8d2d9212b3b4d2dac8
Patrick  Wu <pkwu@Patricks-MBP.lan>
2024-06-05 10:18:36 -0700

    test: encode form body

--- src/ui/components/form/Form.tsx
+++ src/ui/components/form/Form.tsx
@@ -1,39 +1,51 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+
+const encode = (data: { [x: string]: string | number | boolean; }) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
 
 export const Form = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   const handleSubmit = (event: { preventDefault: () => void; target: any; }) => {
     event.preventDefault();
 
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData as any).toString(),
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': 'contact',
+        name: name.trim(),
+        email: email,
+        message: message.trim(),
+      }),
     })
       .then(() => console.log('success'))
       .catch((error) => alert(error));
   };
   return (
     <div>
-      <form name="contact" method="POST" netlify>
+      <form name="contact" method="POST" onSubmit={handleSubmit} netlify>
         <input type="hidden" name="form-name" value="contact"></input>
         <p>
           <label htmlFor="name">Your Name: </label>
-          <input type="text" name="name" />
+          <input type="text" name="name" onChange={(e) => setName(e.target.value)} />
         </p>
 
         <p>
           <label htmlFor="email">Your Email: </label>
-          <input type="email" name="email" />
+          <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} />
         </p>
 
         <p>
-          <label htmlFor="message">Message: </label><textarea name="message"></textarea>
+          <label htmlFor="message">Message: </label>
+          <textarea name="message" onChange={(e) => setMessage(e.target.value)}></textarea>
         </p>
 
-        <button type="submit" onClick={handleSubmit}>Send</button>
+        <button type="submit">Send</button>
       </form>
     </div>
   );
